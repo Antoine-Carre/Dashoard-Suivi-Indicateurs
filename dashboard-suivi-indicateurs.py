b95ac5cdb18dc299135b1963b97fed8a28df6aff
Antoine CARRE <79215772+Antoine-Carre@users.noreply.github.com>
2022-02-19 16:24:57 +0100

    Update dashboard-suivi-indicateurs.py

--- dashboard-suivi-indicateurs.py
+++ dashboard-suivi-indicateurs.py
@@ -2257,9 +2257,14 @@ if categorie_2 == 'Tous':
 
     col2.markdown(html_string_h, unsafe_allow_html=True)
 
+    st.markdown("### **Nombre de recherches réalisées par communes :**")
+    st.markdown("**Attention :**Ici ne sont comptabilisées que les recherches effectuées sur Soliguide pour lesquels les villes, adresses, ou codes postaux ont été saisie dans la recherche. Toutes les recherches par département ou par région ne sont évidemment pas inclues")
+    st.markdown("Vous pouvez choisir la période que vous souhaitez en saisissant une date de début et une date de fin de période")
+    st.markdown("Vous pouvez zoomer de plusieurs façon en sélectionnant les outils sous la flèches en haut à gauche de la carte (par exemple le carré avec la loupe vous permet de sélectionner un territoire")
+
     def main():
         html_temp = """<div class='tableauPlaceholder' id='viz1645283225109' style='position: relative'><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='RecherchesSoliguide&#47;Tableaudebord1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='fr-FR' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1645283225109');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='1000px';vizElement.style.height='827px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='1000px';vizElement.style.height='827px';} else { vizElement.style.width='100%';vizElement.style.height='727px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>"""
-        components.html(html_temp, height = 600)
+        components.html(html_temp, height = 1000)
     if __name__ == "__main__":    
         main()
 
