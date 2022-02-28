aa0a9bf8961a4f363f36e49bf73c731520af1cc9
Antoine CARRE <79215772+Antoine-Carre@users.noreply.github.com>
2022-02-28 14:37:08 +0100

    Update dashboard-suivi-indicateurs.py

--- dashboard-suivi-indicateurs.py
+++ dashboard-suivi-indicateurs.py
@@ -2312,7 +2312,7 @@ if categorie_2 == 'Tous':
     
     df_conversation_crisp = pd.DataFrame(df_crisp['Département de la demande'].value_counts()).reset_index()
     df_conversation_crisp.rename(columns={"index":"Territoire","Département de la demande":"Nombre de conversations"}, inplace=True)
-    st.write(df_conversation_crisp)
+    st.write(df_conversation_crisp[pd.to_numeric(df_conversation_crisp['Territoire'], errors='coerce').notnull()])
     
     figCrisp = px.bar(df_conversation_crisp, x= "Territoire", y="Nombre de conversations")
     st.plotly_chart(figCrisp, use_container_width=True)
