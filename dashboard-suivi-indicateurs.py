263d2244e62a7a48b373da52e71b68567a6cd7a2
Antoine CARRE <79215772+Antoine-Carre@users.noreply.github.com>
2022-03-02 09:12:31 +0100

    Update dashboard-suivi-indicateurs.py

--- dashboard-suivi-indicateurs.py
+++ dashboard-suivi-indicateurs.py
@@ -1696,8 +1696,6 @@ if categorie_2 == 'Tous':
     df_search_users_month.reset_index(inplace=True)
     df_search_users_month = df_search_users_month.iloc[:-1,:]
 
-    st.write(df_search_users_month)
-
     if len(df_search_users_month.columns.to_list()) == 7 or categorie == "France":
 
         figSearch_user = go.Figure(data=[
