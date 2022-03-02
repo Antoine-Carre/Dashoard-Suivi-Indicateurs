0844cd7e4be206f36170553bf68963b78dc73c24
Antoine CARRE <79215772+Antoine-Carre@users.noreply.github.com>
2022-03-02 18:10:43 +0100

    Update dashboard-suivi-indicateurs.py

--- dashboard-suivi-indicateurs.py
+++ dashboard-suivi-indicateurs.py
@@ -2073,8 +2073,9 @@ if categorie_2 == 'Tous':
     if not df_fiches_total.empty:
 
         html_string_d = f"""<br>
-        <center><font face='Helvetica' size='7'>{round((df_sensi_nb[0].count() / df_fiche_serv_on_off[df_fiche_serv_on_off.statut != 0].statut.count())*100, 2)}%</font>
-        <br/><font size='3'>Pourcentage de fiches sensibilisées au moins une fois<br></font></center>"""
+        <center><font face='Helvetica' size='7'>{round((df_sensi_nb["Fiches"].count() / df_fiche_serv_on_off[df_fiche_serv_on_off.statut != 0].statut.count())*100, 2)}%</font>
+        <br/><font size='3'>Pourcentage de fiches sensibilisées au moins une fois<br></font></center>
+        """
     
         col2.markdown(html_string_d, unsafe_allow_html=True)
 
